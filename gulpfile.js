1906acd1c8347c8bb49622c2a574b685572ae63c
Maythee Anegboonlap <natkung@gmail.com>
2014-07-14 16:15:12 +0800

    Add build tasks and rename scheme.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,23 @@
-var gulp = require('gulp')
+var fs = require('fs-extra')
+  , gulp = require('gulp')
+  , path = require('path')
+  , q = require('q')
+  , rm = fs.remove
+  , xcodebuild = require('xcodebuild')
 
-gulp.task('build', function () {
-  console.log ('Dump build')
+var BUILD_DIR = path.join(process.cwd(), 'build')
+
+gulp.task('clean', function (done) {
+  q.nfcall(xcodebuild, 'clean')
+    .then(q.nfcall(rm, BUILD_DIR))
+    .done(done)
+})
+
+gulp.task('build', ['clean'], function () {
+  q.nfcall(xcodebuild, 'build', {
+    scheme: 'GimmieAPI',
+    configuration: 'Release'
+  }).done(done)
 })
 
 gulp.task('default', [ 'build' ])
