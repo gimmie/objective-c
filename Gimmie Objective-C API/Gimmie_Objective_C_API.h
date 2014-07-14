f41ff050b2877c01b88fdc56de2321f1c741b1ee
Maythee Anegboonlap <natkung@gmail.com>
2014-07-14 14:21:55 +0800

    Initial Commit

---
+++ Gimmie Objective-C API/Gimmie_Objective_C_API.h
@@ -0,0 +1,13 @@
+//
+//  Gimmie_Objective_C_API.h
+//  Gimmie Objective-C API
+//
+//  Created by llun on 7/14/14.
+//  Copyright (c) 2014 Gimmieworld pte ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Gimmie_Objective_C_API : NSObject
+
+@end
---
+++ Gimmie Objective-C APITests/Gimmie_Objective_C_APITests.m
@@ -0,0 +1,34 @@
+//
+//  Gimmie_Objective_C_APITests.m
+//  Gimmie Objective-C APITests
+//
+//  Created by llun on 7/14/14.
+//  Copyright (c) 2014 Gimmieworld pte ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Gimmie_Objective_C_APITests : XCTestCase
+
+@end
+
+@implementation Gimmie_Objective_C_APITests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
