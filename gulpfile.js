d26b0bbeeccc4f2f360c82901354a205e1fa9201
Maythee Anegboonlap <natkung@gmail.com>
2014-07-15 15:31:54 +0800

    Add script to create package directory.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 var fs = require('fs-extra')
+  , wrench = require('wrench')
+  , cp = wrench.copyDirSyncRecursive
+  , mkdir = fs.mkdirs
   , gulp = require('gulp')
   , path = require('path')
   , q = require('q')
@@ -6,6 +9,10 @@ var fs = require('fs-extra')
   , xcodebuild = require('xcodebuild')
 
 var BUILD_DIR = path.join(process.cwd(), 'build')
+  , BUILD_LIB = path.join(BUILD_DIR, 'Release-universal')
+  , RELEASE_DIR = path.join(BUILD_DIR, 'gimmie')
+  , HEADER_DIR = path.join(RELEASE_DIR, 'include')
+  , SRC_DIR = path.join(process.cwd(), 'API')
 
 gulp.task('clean', function (done) {
   q.nfcall(xcodebuild, 'clean')
@@ -14,10 +21,43 @@ gulp.task('clean', function (done) {
 })
 
 gulp.task('build', ['clean'], function () {
-  q.nfcall(xcodebuild, 'build', {
+  q.nfcall(xcodebuild, 'archive', {
     scheme: 'GimmieAPI',
     configuration: 'Release'
-  }).done(done)
+  })
+  .then(function () {
+    console.log ('Copy all resources to archive dir')
+
+    cp(BUILD_LIB, RELEASE_DIR)
+    mkdir(HEADER_DIR)
+
+    // Search all header files first
+    var headers = []
+    var queue = [ SRC_DIR ]
+    while (queue.length > 0) {
+      var first = queue.shift()
+
+      var files = fs.readdirSync(first)
+      files.forEach(function (file) {
+        var fullpath = path.join(first, file)
+        if (fs.statSync(fullpath).isDirectory()) {
+          queue.push(fullpath)
+        }
+        else if (/^.*\.h$/.test(file)) {
+          headers.push(fullpath)
+        }
+      })
+    }
+
+    // Copy all headers to release folder
+    headers.forEach(function (header) {
+      var from = header, to = path.join(HEADER_DIR, path.basename(header))
+      fs.createReadStream(from).pipe(fs.createWriteStream(to))
+    })
+
+    return
+  })
+  .done(done)
 })
 
 gulp.task('default', [ 'build' ])
